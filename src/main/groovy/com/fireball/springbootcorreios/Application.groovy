d32e036ee999c1f06d0d5529cec7e2821f4cf7e9
Carlos Menezes <carlos.menezes@moip.com.br>
2014-03-26 09:49:53 -0300

    displaying thymeleaf templates. :v:

--- src/main/groovy/com/fireball/springbootcorreios/Application.groovy
+++ src/main/groovy/com/fireball/springbootcorreios/Application.groovy
@@ -3,9 +3,7 @@ package com.fireball.springbootcorreios
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ComponentScan
 @EnableAutoConfiguration
 class Application {
