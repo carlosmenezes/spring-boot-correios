693a6fb87d89b46fc6d28aa346d19f869bfa894b
carlosmenezes <pacheco.menezes@gmail.com>
2014-04-01 22:01:40 -0300

    stuck with feign...

--- src/main/groovy/com/fireball/springbootcorreios/external/PostalServiceApi.groovy
+++ src/main/groovy/com/fireball/springbootcorreios/external/PostalServiceApi.groovy
@@ -2,12 +2,14 @@ package com.fireball.springbootcorreios.external
 
 import com.fireball.springbootcorreios.external.resources.PostalServiceHistory
 
+import feign.RequestLine;
+
 /**
  * Created by carlos on 27/03/14.
  */
 public interface PostalServiceApi {
 
-//    @Requestli
-    PostalServiceHistory getHistory(PostalServiceRequest request)
+	@RequestLine("POST /sroii_xml.eventos")
+	PostalServiceHistory getHistory(PostalServiceRequest request)
 
 }
--- src/main/groovy/com/fireball/springbootcorreios/services/PostalServiceQueryService.groovy
+++ src/main/groovy/com/fireball/springbootcorreios/services/PostalServiceQueryService.groovy
@@ -1,6 +1,12 @@
 package com.fireball.springbootcorreios.services
 
+import org.jboss.logging.Slf4jLogger;
 import org.springframework.stereotype.Component
+
+import com.fireball.springbootcorreios.external.PostalServiceApi;
+
+import feign.Feign;
+import feign.sax.SAXDecoder;
 
 /**
  * Created by carlos on 27/03/14.
@@ -11,5 +17,12 @@ class PostalServiceQueryService {
     def updateStatus(aPackage) {
 
     }
+	
+	def postalServiceApi() {
+		return Feign.builder()
+			.logger(new Slf4jLogger())
+//			.decoder(SAXDecoder.builder().)
+			.target(PostalServiceApi, "http://websro.correios.com.br/sro_bin")
+	}
 
 }
